7fdfeee5bf331e29af0489fd1514a650b50f1e08
Ethan Richards <ethan.richards@myemail.indwes.edu>
2018-03-23 12:55:22 -0400

    For Class.

--- Assets/Scripts/CheckAnswer.cs
+++ Assets/Scripts/CheckAnswer.cs
@@ -24,11 +24,7 @@ public class CheckAnswer : MonoBehaviour
     public GameObject Triangle;
     public GameObject Circle;
     public GameObject sqCir1;
-    public GameObject sqCir2;
-    public GameObject sqCir3;
     public GameObject sqTri1;
-    public GameObject sqTri2;
-    public GameObject sqTri3;
 
     public Button submitButton;
     public GameObject formulaButton;
@@ -52,6 +48,7 @@ public class CheckAnswer : MonoBehaviour
 
     double sqTri_width = 2;
     double sqTri_height = 3;
+    double sqTri_triHatHeight = 3;
     double sqTri_answer;
 
     double sqCir_width = 2;
@@ -63,48 +60,45 @@ public class CheckAnswer : MonoBehaviour
         var rec_answer = rec_wid * rec_hgt;
         var tri_answer = half * tri_bse * tri_hgt;
         var crc_answer = pi * (crc_rad * crc_rad);
-        var sqTri_answer = (sqTri_width * sqTri_height) * (3 / 2);
-        var sqCir_answer = (sqCir_width * sqTri_height) + (pi * ((sqCir_width / 2) * (sqCir_width / 2)));
+        var sqTri_answer = (sqTri_width * sqTri_height) + ((sqTri_triHatHeight * sqTri_width)/ 2);
+        var sqCir_answer = (sqCir_width * sqCir_height) + (pi * ((sqCir_width / 2) * (sqCir_width / 2)));
         if (userAnswer.text != "")
         {   
-                if (_mode == square_mode && int.Parse(userAnswer.text) == rec_answer)
-                {
-                    outputBox.text = "CORRECT!";
-                    userAnswer.Select();
-                    userAnswer.text = "";
-                    Triangle.SetActive(true);
-                    _mode = tri_mode;
-                }
-                else if (_mode == tri_mode && int.Parse(userAnswer.text) == tri_answer)
-                {
-                    outputBox.text = "CORRECT!";
-                    userAnswer.Select();
-                    userAnswer.text = "";
-                    Circle.SetActive(true);
-                    _mode = cir_mode;
-                }
-                else if (_mode == cir_mode && int.Parse(userAnswer.text) == crc_answer)
-                {
-                    outputBox.text = "Way to go! Thanks for helping me complete those buildings! I need 2 more buildings to complete. (Formulas for areas are attached to button called 'Fomrulas')";
-                    userAnswer.Select();
-                    userAnswer.text = "";
-                    Square.SetActive(false);
-                    Triangle.SetActive(false);
-                    Circle.SetActive(false);
-                    //formulaButton.SetActive(true);
-                    sqTri1.SetActive(true);
-                    sqTri2.SetActive(true);
-                    sqTri3.SetActive(true);
-                    _mode = sqTri_mode;
-                }
-             else if (_mode == sqTri_mode && int.Parse(userAnswer.text) == sqTri_answer)
+            if (_mode == square_mode && int.Parse(userAnswer.text) == rec_answer)
+            {
+                outputBox.text = "CORRECT!";
+                userAnswer.Select();
+                userAnswer.text = "";
+                Triangle.SetActive(true);
+                _mode = tri_mode;
+            }
+            else if (_mode == tri_mode && int.Parse(userAnswer.text) == tri_answer)
+            {
+                outputBox.text = "CORRECT!";
+                userAnswer.Select();
+                userAnswer.text = "";
+                Circle.SetActive(true);
+                _mode = cir_mode;
+            }
+            else if (_mode == cir_mode && int.Parse(userAnswer.text) == crc_answer)
+            {
+                outputBox.text = "Way to go! Thanks for helping me complete those buildings! I need 2 more buildings to complete. (Formulas for areas are attached to button called 'Fomrulas')";
+                userAnswer.Select();
+                userAnswer.text = "";
+                Square.SetActive(false);
+                Triangle.SetActive(false);
+                Circle.SetActive(false);
+                formulaButton.SetActive(true);
+
+                sqTri1.SetActive(true);
+                _mode = sqTri_mode;
+            }
+            else if (_mode == sqTri_mode && int.Parse(userAnswer.text) == sqTri_answer)
             {
                 outputBox.text = "CORRECT!";
                 userAnswer.Select();
                 userAnswer.text = "";
                 sqCir1.SetActive(true);
-                sqCir2.SetActive(true);
-                sqCir3.SetActive(true);
                 _mode = sqCir_mode;
             }
             else if (_mode == sqCir_mode && int.Parse(userAnswer.text) == sqCir_answer)
@@ -112,18 +106,13 @@ public class CheckAnswer : MonoBehaviour
                 outputBox.text = "CORRECT!";
                 userAnswer.Select();
                 userAnswer.text = "";
-                if (Input.GetMouseButtonDown(leftClick))
-                {
-                    this.enabled = false;
-                    SceneManager.LoadScene("SecondLevel");
-                }
+                this.enabled = false;
+                SceneManager.LoadScene("SecondLevel");
             }
-
             else
             {
-                    outputBox.text = "FAILURE! Try again!";
-                }
-                
+                outputBox.text = "FAILURE! Try again!";
+            }
         }
     }
 }
