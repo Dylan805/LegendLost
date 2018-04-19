dbf453bc34a957148b9d42b608b7e7aff84885e3
Ethan Richards <ethan.richards@myemail.indwes.edu>
2018-04-19 14:03:11 -0400

    Final Update 2 Electric Boogaloo

--- Assets/Scripts/Level3Script.cs
+++ Assets/Scripts/Level3Script.cs
@@ -9,7 +9,7 @@ public class Level3Script : MonoBehaviour
     public bool selected = false; //Selected flag
     public float value; //Hard coded value for boxes, will be changed later
 
-    private const float SWITDTH = 0.15F; //Scale Width
+    private const float SWITDTH = 0.35F; //Scale Width
     private const float SHEIGHT = 0.15F; //Scale Hight
     private const float DISPLACE = 0.5F; //Displacement fix for scale adjust
 
